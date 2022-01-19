28da7b48dc5146931e2b47eb4b5dc4517ba4bbc8
Arthurcommit <66469924+Arthurcommit@users.noreply.github.com>
2022-01-20 00:01:56 +0100

    Update VueFormulate.vue

--- src/components/VueFormulate.vue
+++ src/components/VueFormulate.vue
@@ -11,11 +11,11 @@
 						<div id="intro2">Inscription</div> 
 					</div>
 					<FormulateForm v-model="formValues" @submit="handleSubmit" style="text-align:center">
-						<FormulateInput placeholder="Firstname" type="text" id="firstname" name="firstname" validation="required" style="margin:auto"  />
-						<FormulateInput placeholder="Lastname" type="text" id="lastname" name="lastname" validation="required" style="margin:auto"  />
+						<FormulateInput placeholder="First name" type="text" id="firstname" name="firstname" validation="required" style="margin:auto"  />
+						<FormulateInput placeholder="Last name" type="text" id="lastname" name="lastname" validation="required" style="margin:auto"  />
 						<FormulateInput placeholder="Email" type="text" id="mail" name="Email" validation="required|email" style="margin:auto" />
 						<FormulateInput placeholder="Confirm Email" type="text" id="confirm_mail" name="Confirm" validation="required" style="margin:auto" />
-						<FormulateInput placeholder="Password" type="password" id="mdp" name="Password" validation="required" style="margin:auto" /><br>
+						<FormulateInput placeholder="New Password" type="password" id="mdp" name="Password" validation="required" style="margin:auto" /><br>
 							<p id="intro4">En cliquant sur inscription, vous acceptez nos Conditions 
 							et indiquez que vous avez lu notre Politique d'utilisation des données, y compris notre Utilisation des Cookies.
 							Vous pourrez recevoir des notifications par texto de la part de Facebook et pouvez vous désabonner à tout moment.
